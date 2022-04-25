e20729bd780c669743367b4862bcc9e4e2406c28
wow-rainbow <ramsey7101@gmail.com>
2022-04-25 22:18:16 +0800

    first

---
+++ src/guess_game.rs
@@ -0,0 +1,41 @@
+use std::io;
+use std::cmp::Ordering;
+use rand::Rng;
+
+fn main() {
+    // --snip--
+
+    println!("guess the number!");
+
+    let secret_number = rand::thread_rng().gen_range(1..101);
+
+    loop {
+        println!("input your number.");
+
+        let mut guess= String::new();
+
+        io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => continue,
+        };
+
+        println!("You guess: {}", guess);
+
+        match guess.cmp(&secret_number) {
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too big"),
+            Ordering::Equal => {
+                println!("You win!");
+                break;
+            }
+        }
+    }
+    
+    
+    
+
+}
